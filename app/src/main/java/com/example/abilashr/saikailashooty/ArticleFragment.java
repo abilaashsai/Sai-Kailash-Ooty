8c6899eb8e5ca002056e16ed64f9f2ffe926bd86
abilashr <abilaashsai@gmail.com>
2017-04-29 00:17:47 +0530

    initialize sai kailash project with hamburger menu and basic fragments

---
+++ app/src/main/java/com/example/abilashr/saikailashooty/ArticleFragment.java
@@ -0,0 +1,34 @@
+package com.example.abilashr.saikailashooty;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ArticleFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ArticleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ArticleFragment extends Fragment {
+    public ArticleFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_article, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/abilashr/saikailashooty/LocationFragment.java
@@ -0,0 +1,33 @@
+package com.example.abilashr.saikailashooty;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LocationFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LocationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LocationFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location, container, false);
+    }
+}
