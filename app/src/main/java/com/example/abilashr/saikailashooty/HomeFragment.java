1d25db3068e5223695a18656b005602fff8ff5d4
abilashr <abilaashsai@gmail.com>
2017-05-01 16:11:06 +0530

    use content providers for accessing and storing data

--- app/src/main/java/com/example/abilashr/saikailashooty/HomeFragment.java
+++ app/src/main/java/com/example/abilashr/saikailashooty/HomeFragment.java
@@ -1,19 +1,16 @@
 package com.example.abilashr.saikailashooty;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.abilashr.saikailashooty.data.ThoughtContract;
-import com.example.abilashr.saikailashooty.data.ThoughtDbHelper;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,10 +20,14 @@ import com.google.firebase.database.ValueEventListener;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static com.example.abilashr.saikailashooty.data.ThoughtContract.*;
+
 public class HomeFragment extends Fragment {
-    @BindView(R.id.thoughtitle) TextView thoughtTitle;
-    @BindView(R.id.thoughtdetail) TextView thoughtDetail;
-    private SQLiteDatabase sqLiteDatabase;
+    @BindView(R.id.thoughtitle)
+    TextView thoughtTitle;
+    @BindView(R.id.thoughtdetail)
+    TextView thoughtDetail;
+
     Cursor cursor;
 
     public HomeFragment() {
@@ -39,32 +40,21 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, rootView);
 
-        ThoughtDbHelper thoughtDbHelper = new ThoughtDbHelper(getActivity());
-        sqLiteDatabase = thoughtDbHelper.getWritableDatabase();
-        cursor = retrieveThoughtsFromDatabase();
-        if(cursor.getCount() != 0) {
-            cursor.moveToNext();
-            thoughtTitle.setText(cursor.getString(cursor.getColumnIndex(ThoughtContract.ThoughtEntry.THOUGHT_TITLE)));
-            thoughtDetail.setText(cursor.getString(cursor.getColumnIndex(ThoughtContract.ThoughtEntry.THOUGHT_DETAIL)));
-        }
+        populateThoughtsFromDatabaseIfExist();
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("thought");
+        DatabaseReference myRef = database.getReference(getResources().getString(R.string.thought));
+
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String firebaseThoughtTitle = dataSnapshot.child("title").getValue(String.class);
-                String firebaseThoughtDetails = dataSnapshot.child("data").getValue(String.class);
+                String firebaseThoughtTitle = dataSnapshot.child(getResources().getString(R.string.title)).getValue(String.class);
+                String firebaseThoughtDetails = dataSnapshot.child(getResources().getString(R.string.data)).getValue(String.class);
 
-                if(cursor.getCount() != 0 && cursor.getString(cursor.getColumnIndex(ThoughtContract.ThoughtEntry.THOUGHT_TITLE)).equals(firebaseThoughtTitle)) {
-
-                } else {
-                    thoughtTitle.setText(firebaseThoughtTitle);
-                    thoughtDetail.setText(firebaseThoughtDetails);
+                if(cursor.getCount() != 0 && !cursor.getString(cursor.getColumnIndex(ThoughtEntry.THOUGHT_TITLE)).equals(firebaseThoughtTitle)) {
                     addThoughtsIntoDatabase(firebaseThoughtTitle, firebaseThoughtDetails);
-
+                    updateUI();
                 }
-
             }
 
             @Override
@@ -76,21 +66,28 @@ public class HomeFragment extends Fragment {
         return rootView;
     }
 
-    private Cursor retrieveThoughtsFromDatabase() {
-        return sqLiteDatabase.query(ThoughtContract.ThoughtEntry.TABLE_NAME,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null);
+    private void updateUI() {
+        thoughtTitle.setText(cursor.getString(cursor.getColumnIndex(ThoughtEntry.THOUGHT_TITLE)));
+        thoughtDetail.setText(cursor.getString(cursor.getColumnIndex(ThoughtEntry.THOUGHT_DETAIL)));
     }
 
-    private long addThoughtsIntoDatabase(String title, String details) {
-        sqLiteDatabase.execSQL("delete from " + ThoughtContract.ThoughtEntry.TABLE_NAME);
+    private void populateThoughtsFromDatabaseIfExist() {
+        cursor = getContentResolver().query(ThoughtEntry.CONTENT_URI, null, null, null, null);
+        if(cursor.getCount() != 0) {
+            cursor.moveToNext();
+            updateUI();
+        }
+    }
+
+    private void addThoughtsIntoDatabase(String title, String details) {
+        getContentResolver().delete(ThoughtEntry.CONTENT_URI, null, null);
         ContentValues contentValues = new ContentValues();
-        contentValues.put(ThoughtContract.ThoughtEntry.THOUGHT_TITLE, title);
-        contentValues.put(ThoughtContract.ThoughtEntry.THOUGHT_DETAIL, details);
-        return sqLiteDatabase.insert(ThoughtContract.ThoughtEntry.TABLE_NAME, null, contentValues);
+        contentValues.put(ThoughtEntry.THOUGHT_TITLE, title);
+        contentValues.put(ThoughtEntry.THOUGHT_DETAIL, details);
+        getContentResolver().insert(ThoughtEntry.CONTENT_URI, contentValues);
+    }
+
+    private ContentResolver getContentResolver() {
+        return getActivity().getContentResolver();
     }
 }
--- app/src/main/java/com/example/abilashr/saikailashooty/data/ThoughtContract.java
+++ app/src/main/java/com/example/abilashr/saikailashooty/data/ThoughtContract.java
@@ -1,10 +1,15 @@
 package com.example.abilashr.saikailashooty.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class ThoughtContract {
+    public static final String AUTHORITY = "com.example.abilashr.saikailashooty";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    public static final String PATH_TASKS = "title";
 
     public static final class ThoughtEntry implements BaseColumns{
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
         public static final String TABLE_NAME = "thought";
         public static final String THOUGHT_TITLE = "title";
         public static final String THOUGHT_DETAIL = "detail";
