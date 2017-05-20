61021a8d559f515dfb1459a8f6289f271ee51bd1
abilashr <abilaashsai@gmail.com>
2017-05-20 14:02:26 +0530

    update gradle and build tools

--- app/src/main/java/com/example/abilashr/saikailashooty/LocationFragment.java
+++ app/src/main/java/com/example/abilashr/saikailashooty/LocationFragment.java
@@ -1,24 +1,45 @@
 package com.example.abilashr.saikailashooty;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class LocationFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_location, container, false);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng saiKailash = new LatLng(11.376741, 76.749979);
+        mMap.addMarker(new MarkerOptions().position(saiKailash).title("Sai Kailash"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(saiKailash));
     }
 }
