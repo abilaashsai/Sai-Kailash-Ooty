f09893c866e37caae6ec73614545957ab9f6ad36
abilashr <abilaashsai@gmail.com>
2017-06-17 01:16:03 +0530

    change icon of Sai Kailash

--- app/src/androidTest/java/com/saikailash/ooty/organization/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/saikailash/ooty/organization/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.abilashr.saikailashooty", appContext.getPackageName());
+        assertEquals("com.saikailash.ooty.saikailashooty", appContext.getPackageName());
     }
 }
